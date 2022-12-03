bbc94efceb5f616c0af9f8b657ac2ac071300722
boldty <rasber96@gmail.com>
2022-12-03 12:36:24 +0100

    io refactor

--- io.js
+++ io.js
@@ -1,10 +1,12 @@
-import { run2 as run } from './apps/app3.js';
+import { run1, run2 } from './apps/app3.js';
 
 function read() {
   const inputElement = document.getElementById('input');
-  const outputElement = document.getElementById('output');
+  const outputElement1 = document.getElementById('output1');
+  const outputElement2 = document.getElementById('output2');
 
-  outputElement.value = run(inputElement.value);
+  outputElement1.value = run1(inputElement.value);
+  outputElement2.value = run2(inputElement.value);
 }
 
 window.read = read;

6f428ec9ade80c338bb28b638812366b77caf6f4
boldty <rasber96@gmail.com>
2022-12-08 08:44:10 +0100

    Day8

--- apps/app8.js
+++ apps/app8.js
@@ -1,13 +1,5 @@
 function run1(input) {
-  let matrix = [];
-  input.split('\n').forEach((line) => {
-    let array = [];
-    line.split('').forEach((tree) => {
-      array.push(tree);
-    });
-    console.log(array);
-    matrix.push(array);
-  });
+  let matrix = createMatrix(input);
 
   let sum = 0;
   for (let x = 0; x < matrix.length; x++) {
@@ -21,19 +13,19 @@ function run1(input) {
         sum++;
         continue;
       }
-      if (1 < checkLosLeft(matrix[x][y], matrix, x, y - 1, 1)) {
+      if (checkLosLeft(matrix[x][y], matrix, x, y - 1)) {
         sum++;
         continue;
       }
-      if (1 < checkLosRight(matrix[x][y], matrix, x, y + 1, 1)) {
+      if (checkLosRight(matrix[x][y], matrix, x, y + 1)) {
         sum++;
         continue;
       }
-      if (1 < checkLosUp(matrix[x][y], matrix, x + 1, y, 1)) {
+      if (checkLosUp(matrix[x][y], matrix, x + 1, y)) {
         sum++;
         continue;
       }
-      if (1 < checkLosDown(matrix[x][y], matrix, x - 1, y, 1)) {
+      if (checkLosDown(matrix[x][y], matrix, x - 1, y)) {
         sum++;
         continue;
       }
@@ -42,48 +34,9 @@ function run1(input) {
   return sum;
 }
 
-function checkLosLeft(current, matrix, x, y, distance) {
-  if (y < 0) {
-    return distance;
-  }
-  if (matrix[x][y] >= current) {
-    return distance + 1;
-  }
-  return checkLosLeft(current, matrix, x, y - 1, distance + 1);
-}
-
-function checkLosRight(current, matrix, x, y, distance) {
-  if (y >= matrix.length) {
-    return distance;
-  }
-  if (matrix[x][y] >= current) {
-    return distance + 1;
-  }
-  return checkLosRight(current, matrix, x, y + 1, distance + 1);
-}
-
-function checkLosUp(current, matrix, x, y, distance) {
-  if (x >= matrix.length) {
-    return distance;
-  }
-  if (matrix[x][y] >= current) {
-    return distance + 1;
-  }
-  return checkLosUp(current, matrix, x + 1, y, distance + 1);
-}
-
-function checkLosDown(current, matrix, x, y, distance) {
-  if (x < 0) {
-    return distance;
-  }
-  if (matrix[x][y] >= current) {
-    return distance + 1;
-  }
-  return checkLosDown(current, matrix, x - 1, y, distance + 1);
-}
-
-function run2(input) {
+function createMatrix(input) {
   let matrix = [];
+
   input.split('\n').forEach((line) => {
     let array = [];
     line.split('').forEach((tree) => {
@@ -93,34 +46,107 @@ function run2(input) {
     matrix.push(array);
   });
 
+  return matrix;
+}
+
+function checkLosLeft(current, matrix, x, y) {
+  if (y < 0) {
+    return true;
+  }
+  if (matrix[x][y] >= current) {
+    return false;
+  }
+  return checkLosLeft(current, matrix, x, y - 1);
+}
+
+function checkLosRight(current, matrix, x, y) {
+  if (y >= matrix.length) {
+    return true;
+  }
+  if (matrix[x][y] >= current) {
+    return false;
+  }
+  return checkLosRight(current, matrix, x, y + 1);
+}
+
+function checkLosUp(current, matrix, x, y) {
+  if (x >= matrix.length) {
+    return true;
+  }
+  if (matrix[x][y] >= current) {
+    return false;
+  }
+  return checkLosUp(current, matrix, x + 1, y);
+}
+
+function checkLosDown(current, matrix, x, y) {
+  if (x < 0) {
+    return true;
+  }
+  if (matrix[x][y] >= current) {
+    return false;
+  }
+  return checkLosDown(current, matrix, x - 1, y);
+}
+
+function run2(input) {
+  let matrix = createMatrix(input);
+
   let currentMax = 0;
   for (let x = 0; x < matrix.length; x++) {
     for (let y = 0; y < matrix[0].length; y++) {
-      if (
-        x <= 0 ||
-        y <= 0 ||
-        x >= matrix.length - 1 ||
-        y >= matrix.length - 1
-      ) {
-        continue;
-      }
-      const score1 = checkLosLeft(matrix[x][y], matrix, x, y - 1, 0);
-      const score2 = checkLosRight(matrix[x][y], matrix, x, y + 1, 0);
-      const score3 = checkLosUp(matrix[x][y], matrix, x + 1, y, 0);
-      const score4 = checkLosDown(matrix[x][y], matrix, x - 1, y, 0);
+      const score1 = calculateDistanceLeft(matrix[x][y], matrix, x, y - 1, 0);
+      const score2 = calculateDistanceRight(matrix[x][y], matrix, x, y + 1, 0);
+      const score3 = calculateDistanceUp(matrix[x][y], matrix, x + 1, y, 0);
+      const score4 = calculateDistanceDown(matrix[x][y], matrix, x - 1, y, 0);
 
       const totalScore = score1 * score2 * score3 * score4;
       if (totalScore > currentMax) {
-        console.log(x + ' ' + y);
-        console.log(score1);
-        console.log(score2);
-        console.log(score3);
-        console.log(score4);
         currentMax = totalScore;
       }
     }
   }
   return currentMax;
+}
+
+function calculateDistanceLeft(current, matrix, x, y, distance) {
+  if (y < 0) {
+    return distance;
+  }
+  if (matrix[x][y] >= current) {
+    return distance + 1;
+  }
+  return calculateDistanceLeft(current, matrix, x, y - 1, distance + 1);
+}
+
+function calculateDistanceRight(current, matrix, x, y, distance) {
+  if (y >= matrix.length) {
+    return distance;
+  }
+  if (matrix[x][y] >= current) {
+    return distance + 1;
+  }
+  return calculateDistanceRight(current, matrix, x, y + 1, distance + 1);
+}
+
+function calculateDistanceUp(current, matrix, x, y, distance) {
+  if (x >= matrix.length) {
+    return distance;
+  }
+  if (matrix[x][y] >= current) {
+    return distance + 1;
+  }
+  return calculateDistanceUp(current, matrix, x + 1, y, distance + 1);
+}
+
+function calculateDistanceDown(current, matrix, x, y, distance) {
+  if (x < 0) {
+    return distance;
+  }
+  if (matrix[x][y] >= current) {
+    return distance + 1;
+  }
+  return calculateDistanceDown(current, matrix, x - 1, y, distance + 1);
 }
 
 export { run1, run2 };

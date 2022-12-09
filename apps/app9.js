a1bc7a45a33c2cbeea261b104afb181e09a44ef8
boldty <rasber96@gmail.com>
2022-12-09 14:51:06 +0100

    Day 9  - Part1

--- apps/app9.js
+++ apps/app9.js
@@ -1,93 +1,49 @@
-let tailX = 0;
-let tailY = 0;
-let headX = 0;
-let headY = 0;
-let firstMove = true;
-
 function run1(input) {
+  let tailX = 0;
+  let tailY = 0;
+  let headX = 0;
+  let headY = 0;
   let visitedCells = [getString(tailX, tailY)];
 
   input.split('\n').forEach((line) => {
     let opperation = line.split(' ');
     let dirrection = opperation[0];
     let moves = opperation[1];
-    let isXMove = true;
-    firstMove = true;
     for (let move = 1; move <= moves; move++) {
-      const string = getString(tailX, tailY);
-      if (visitedCells.indexOf(string) === -1) {
-        visitedCells.push(string);
-      }
-      // Head
-      //console.log(getString(headX, headY));
-      //console.log(getString(tailX, tailY));
-      //console.log(dirrection);
       if (dirrection == 'R') {
         headX++;
         if (headX > tailX + 1) {
           tailX++;
-        } else {
-          firstMove = false;
-          continue;
+          tailY = headY;
         }
-        updateY();
-      }
-      if (dirrection == 'L') {
+      } else if (dirrection == 'L') {
         headX--;
         if (headX < tailX - 1) {
           tailX--;
-        } else {
-          firstMove = false;
-          continue;
+          tailY = headY;
         }
-        updateY();
-      }
-      if (dirrection == 'U') {
+      } else if (dirrection == 'U') {
         headY++;
         if (headY > tailY + 1) {
           tailY++;
-        } else {
-          firstMove = false;
-          continue;
+          tailX = headX;
         }
-        updateX();
-      }
-      if (dirrection == 'D') {
+      } else if (dirrection == 'D') {
         headY--;
         if (headY < tailY - 1) {
           tailY--;
-        } else {
-          firstMove = false;
-          continue;
+          tailX = headX;
         }
-        updateX();
+      }
+
+      const string = getString(tailX, tailY);
+      if (visitedCells.indexOf(string) === -1) {
+        visitedCells.push(string);
       }
     }
-    firstMove = false;
-
-    const string = getString(tailX, tailY);
-    if (visitedCells.indexOf(string) === -1) {
-      visitedCells.push(string);
-    }
   });
-  console.log(visitedCells);
 
   return visitedCells.length;
-}
-
-function updateY() {
-  if (firstMove) {
-    return;
-  }
-  tailY = headY;
-}
-
-function updateX() {
-  if (firstMove) {
-    firstMove = false;
-    return;
-  }
-  tailX = headX;
 }
 
 function getString(x, y) {
